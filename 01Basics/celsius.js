9bb21d2927ce41bf95ba910d834b0a9c89647c4d
roony4 <roony4>
2019-02-09 09:06:40 +0530

    1st init basics

---
+++ 01Basics/celsius.js
@@ -0,0 +1,23 @@
+var score=100;
+score = 130
+const bonus = 50
+
+var finalScore=score+bonus
+console.log(finalScore)  // 130
+
+
+var finalScore2=(score+bonus)*1.8
+console.log(finalScore2)  // 324
+
+
+//BODMAS
+var complex=4+4*5/2
+console.log(complex)  //14
+
+
+
+//temprature in celesius
+
+let tempInF=100
+let celsius=(tempInF-32)*5/9
+console.log(celsius)
---
+++ 01Basics/variables.js
@@ -0,0 +1,35 @@
+let name = 'Ruwan';
+let score = 100;
+
+console.log(name);  //Ruwan
+console.log(score); //100
+
+let no1 = '10';
+let no2 = '23';
+let total = no1 + no2;
+console.log(total); //1023
+
+
+
+let no3 = 10;
+let no4 = 20;
+let total2 = no3 + no4; 
+console.log(total2); //33
+
+
+
+let fname='Ruwan'
+let lname = 'Madushanka'
+
+console.log(fname+' '+lname); // Ruwan Madushanka
+
+const speed = 100; //this cant be change
+
+
+
+
+
+
+ //js
+ //don't initialize data types like int,float,string
+ //simply say let
